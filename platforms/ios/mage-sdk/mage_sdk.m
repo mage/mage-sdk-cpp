366fa224ad045eb64e61fd1020cac1673f13382f
stelcheck <mtrudel@wizcorp.jp>
2014-06-29 01:05:10 +0900

    * IOS integration

---
+++ platforms/ios/mage-sdk/mage_sdk.m
@@ -0,0 +1,13 @@
+//
+//  mage_sdk.m
+//  mage-sdk
+//
+//  Created by Marc Trudel on 6/29/14.
+//  Copyright (c) 2014 Wizcorp. All rights reserved.
+//
+
+#import "mage_sdk.h"
+
+@implementation mage_sdk
+
+@end
---
+++ platforms/ios/mage-sdkTests/mage_sdkTests.m
@@ -0,0 +1,34 @@
+//
+//  mage_sdkTests.m
+//  mage-sdkTests
+//
+//  Created by Marc Trudel on 6/29/14.
+//  Copyright (c) 2014 Wizcorp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mage_sdkTests : XCTestCase
+
+@end
+
+@implementation mage_sdkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
